1bd56427351ebad03936ef95f83750efcb8fef3d
dernasherbrezon <rodionovamp@mail.ru>
2017-11-12 18:25:36 +0000

    update dashboard status

--- src/components/pages/status/overview.vue
+++ src/components/pages/status/overview.vue
@@ -8,6 +8,34 @@
 
 <script>
 export default {
-  name: 'status'
+  name: 'status',
+  mounted: function () {
+    var dashboard = document.getElementById('dashboard')
+    dashboard.addEventListener('load', function () {
+      update()
+    }, false)
+    function update () {
+      var value = {status: 'ERROR', message: 'Error message'}
+      var color
+      var property = 'rtldongle'
+      if (value.status === 'SUCCESS') {
+        color = '#3c763d'
+      } else if (value.status === 'ERROR') {
+        color = '#a94442'
+      } else {
+        color = '#777'
+      }
+
+      var elem = dashboard.contentDocument.getElementById(property)
+      if (elem !== null) {
+        elem.style.fill = color
+        if (value.status === 'ERROR') {
+          elem.innerHTML = '<title>' + value.message + '</title>'
+        } else {
+          elem.innerHTML = ''
+        }
+      }
+    }
+  }
 }
 </script>
