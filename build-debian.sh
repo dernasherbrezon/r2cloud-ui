654937500c0eac3161e439643b9d11986a1c1651
dernasherbrezon <rodionovamp@mail.ru>
2017-12-22 13:21:46 +0000

    switch to aws back

--- build-debian.sh
+++ build-debian.sh
@@ -5,3 +5,5 @@ DATE=`date +%s`
 echo "Version: ${DATE}" >> debian/DEBIAN/control
 
 dpkg-deb --build debian r2cloud-ui-${DATE}.deb
+
+deb-s3 upload -a armhf -c r2cloud --access-key-id=${AWS_ACCESS_KEY} --secret-access-key=${AWS_SECRET_ACCESS_KEY}  -m main --sign=27679FBF --gpg-options="--passphrase ${GPG_PASSPHRASE}" --bucket r2cloud *.deb
