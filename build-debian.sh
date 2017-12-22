bef1a2f0861c317f5b7225c5af0f8d6c3e8b6c85
dernasherbrezon <rodionovamp@mail.ru>
2017-12-22 13:26:40 +0000

    import key

--- build-debian.sh
+++ build-debian.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+
 DATE=`date +%s`
 
 echo "Version: ${DATE}" >> debian/DEBIAN/control
 
 dpkg-deb --build debian r2cloud-ui-${DATE}.deb
+
+openssl aes-256-cbc -K $encrypted_ace8744cfeee_key -iv $encrypted_ace8744cfeee_iv -in codesigning.asc.enc -out codesigning.asc -d
+gpg --fast-import codesigning.asc
 
 deb-s3 upload -a armhf -c r2cloud --access-key-id=${AWS_ACCESS_KEY} --secret-access-key=${AWS_SECRET_ACCESS_KEY}  -m main --sign=27679FBF --gpg-options="--passphrase ${GPG_PASSPHRASE}" --bucket r2cloud *.deb
