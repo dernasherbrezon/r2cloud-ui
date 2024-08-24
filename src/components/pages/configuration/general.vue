2c9e17f3f945a0574b79524e5814a86f313a43a9
dernasherbrezon <dernasherbrezon@gmail.com>
2024-08-24 18:24:54 +0100

    support altitude and GPS

--- src/components/pages/configuration/general.vue
+++ src/components/pages/configuration/general.vue
@@ -14,29 +14,48 @@
           <div class="col-md-4">
             <div class="form-group" :class="{'has-danger': errors.has('lat') }">
               <label for="lat">Latitude</label>
-              <input type="text" id="lat" name="lat" :class="{'is-invalid': errors.has('lat') }" v-validate="'required|decimal'" class="form-control" v-model="lat">
+              <input type="text" id="lat" name="lat" :class="{'is-invalid': errors.has('lat') }" :disabled="entity.locationAuto" v-validate="'required|decimal'" class="form-control" v-model.number="entity.lat">
               <div class="invalid-feedback" v-if="errors.has('lat')">{{ errors.first('lat') }}</div>
             </div>
           </div>
           <div class="col-md-4">
             <div class="form-group" :class="{'has-danger': errors.has('lng') }">
               <label for="lng">Longitude</label>
-              <input type="text" id="lng" name="lng" :class="{'is-invalid': errors.has('lng') }" v-validate="'required|decimal'" class="form-control" v-model="lng">
+              <input type="text" id="lng" name="lng" :class="{'is-invalid': errors.has('lng') }" :disabled="entity.locationAuto" v-validate="'required|decimal'" class="form-control" v-model.number="entity.lng">
               <div class="invalid-feedback" v-if="errors.has('lng')">{{ errors.first('lng') }}</div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="form-group">
-                <label for="detect">&nbsp;</label>
-                <button id="detect" type="button" class="btn btn-light" v-on:click="detectLocation" style="display: block;">Detect</button>
             </div>
           </div>
         </div>
         <div class="row">
           <div class="col-md-4">
+            <div class="form-group" :class="{'has-danger': errors.has('alt') }">
+              <label for="alt">Altitude</label>
+              <input type="text" id="alt" name="alt" :class="{'is-invalid': errors.has('alt') }" :disabled="entity.locationAuto" v-validate="'decimal'" class="form-control" v-model.number="entity.alt">
+              <div class="invalid-feedback" v-if="errors.has('alt')">{{ errors.first('alt') }}</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+                <label for="detect">&nbsp;</label>
+                <button id="detect" type="button" class="btn btn-light" :disabled="entity.locationAuto" v-on:click="detectLocation" style="display: block;">Detect from Browser</button>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+	        <div class="form-check">
+	            <input class="form-check-input" type="checkbox" id="locationAuto" v-model="entity.locationAuto">
+	            <label class="form-check-label" for="locationAuto"> Use GPS</label>
+                <small id="locationAutoHelp" class="form-text text-muted">
+         	      	Use connected GPS devices to automatically determine location on the startup. Currently supported Linux and <a href="https://gpsd.gitlab.io/gpsd/index.html" target="blank">GPSD</a>.
+                </small>
+	        </div>
+        </div>
+        <hr/>
+        <div class="row">
+          <div class="col-md-4">
               <label for="retentionMaxSizeBytes">Retention</label>
               <div class="input-group md-4">
-                  <input type="number" step="0.001" id="retentionMaxSizeBytes" name="retentionMaxSizeBytes" :class="{'is-invalid': errors.has('retentionMaxSizeBytes') }" v-validate="'required|decimal'" class="form-control" v-model.number="retentionMaxSizeBytes">
+                  <input type="number" step="0.001" id="retentionMaxSizeBytes" name="retentionMaxSizeBytes" :class="{'is-invalid': errors.has('retentionMaxSizeBytes') }" v-validate="'required|decimal'" class="form-control" v-model.number="entity.retentionMaxSizeBytes">
                   <div class="input-group-append">
                       <span class="input-group-text" id="basic-addon2">GiB</span>
                   </div>
@@ -49,7 +68,7 @@
           <div class="col-md-4">
             <div class="form-group" :class="{'has-danger': errors.has('retentionRawCount') }">
               <label for="retentionRawCount">Max I/Q files</label>
-              <input type="text" id="retentionRawCount" name="retentionRawCount" :class="{'is-invalid': errors.has('retentionRawCount') }" v-validate="'required|decimal'" class="form-control" v-model="retentionRawCount">
+              <input type="text" id="retentionRawCount" name="retentionRawCount" :class="{'is-invalid': errors.has('retentionRawCount') }" v-validate="'required|decimal'" class="form-control" v-model.number="entity.retentionRawCount">
               <div class="invalid-feedback" v-if="errors.has('retentionRawCount')">{{ errors.first('retentionRawCount') }}</div>
               <small id="retentionRawCountHelp" class="form-text text-muted">
               	Number of raw I/Q files to keep. They can be used for manual processing and analysis. They are the biggest disk space consumers.
@@ -60,14 +79,14 @@
         <hr/>
         <div class="form-group">
 	        <div class="form-check">
-	            <input class="form-check-input" type="checkbox" id="gridCheck" v-model="autoUpdate">
+	            <input class="form-check-input" type="checkbox" id="gridCheck" v-model="entity.autoUpdate">
 	            <label class="form-check-label" for="gridCheck"> Auto-update enabled</label>
 	        </div>
         </div>
         <hr/>
         <div class="form-group">
 	        <div class="form-check">
-	            <input class="form-check-input" type="checkbox" id="presentationMode" v-model="presentationMode">
+	            <input class="form-check-input" type="checkbox" id="presentationMode" v-model="entity.presentationMode">
 	            <label class="form-check-label" for="presentationMode"> Presentation mode enabled</label>
 				<small id="presentationModeHelp" class="form-text text-muted">
 				  	This will allow access to the following data WITHOUT username and password:
@@ -86,28 +105,23 @@
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
   name: 'general',
   data () {
     return {
-      lat: '',
-      lng: '',
-      retentionMaxSizeBytes: 0,
-      retentionRawCount: 0,
-      autoUpdate: false,
-      presentationMode: false,
+      entity: {},
       submitting: false
     }
   },
   mounted () {
     const vm = this
     vm.$http.get('/admin/config/general').then(function (response) {
-      vm.lat = response.data.lat
-      vm.lng = response.data.lng
-      vm.autoUpdate = response.data.autoUpdate
-      vm.presentationMode = response.data.presentationMode
-      vm.retentionMaxSizeBytes = response.data.retentionMaxSizeBytes / 1024 / 1024 / 1024
-      vm.retentionRawCount = response.data.retentionRawCount
+      if (response.data) {
+        response.data.retentionMaxSizeBytes = response.data.retentionMaxSizeBytes / 1024 / 1024 / 1024
+      } 
+      vm.entity = response.data
     })
   },
   methods: {
@@ -115,8 +129,9 @@ export default {
       const vm = this
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-          vm.lat = position.coords.latitude
-          vm.lng = position.coords.longitude
+          vm.entity.lat = position.coords.latitude
+          vm.entity.lng = position.coords.longitude
+          vm.entity.alt = position.coords.altitude
         })
       }
     },
@@ -134,14 +149,12 @@ export default {
       }
       this.submitting = true
       const vm = this
-      vm.$http.post('/admin/config/general', {
-        lat: parseFloat(vm.lat),
-        lng: parseFloat(vm.lng),
-        autoUpdate: vm.autoUpdate,
-        presentationMode: vm.presentationMode,
-        retentionMaxSizeBytes: vm.retentionMaxSizeBytes * 1024 * 1024 * 1024,
-        retentionRawCount: vm.retentionRawCount
-      }).then(function (response) {
+      var toSubmit = Vue.util.extend({}, vm.entity)
+      toSubmit.retentionMaxSizeBytes = toSubmit.retentionMaxSizeBytes * 1024 * 1024 * 1024
+      if( toSubmit.alt === '') {
+        delete toSubmit.alt
+      }
+      vm.$http.post('/admin/config/general', toSubmit).then(function (response) {
         vm.submitting = false
         vm.$router.push('/admin/restart')
       }).catch(function (error) {
